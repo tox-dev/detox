fb81e657611db768ea64bb790dadaf6baaf8629c
holger krekel <holger@merlinux.eu>
2011-11-29 17:04:56 +0000

    also test stderr

--- tests/test_detox.py
+++ tests/test_detox.py
@@ -2,25 +2,27 @@ import pytest
 from detox.proc import StreamProcess
 import eventlet
 
-@pytest.mark.parametrize(("stream", "value",), [
-    ("stdout", "a", ),
-    ("stdout", "a\n", ),
-    ("stderr", "b13\n17", ),
-])
-def test_subprocess_outstreams(stream, value):
-    sp = StreamProcess(["python", "-c",
-        "import sys ; sys.%s.write(%r)" % (stream, value)])
-    l = []
-    sp.copy_outstream(stream, l.append)
-    sp.wait_outstreams()
-    result = "".join(l)
-    assert result == value
+class TestStreamProcess:
+    @pytest.mark.parametrize(("stream", "value",), [
+        ("stdout", "a", ),
+        ("stdout", "a\n", ),
+        ("stderr", "b13\n17", ),
+    ])
+    def test_outstreams(self, stream, value):
+        sp = StreamProcess(["python", "-c",
+            "import sys ; sys.%s.write(%r)" % (stream, value)])
+        l = []
+        sp.copy_outstream(stream, l.append)
+        sp.wait_outstreams()
+        result = "".join(l)
+        assert result == value
 
-def test_subprocess_linetimeout():
-    sp = StreamProcess(["python", "-c",
-        "import time ; time.sleep(5)"], linetimeout=0.01)
-    sp.copy_outstream("stdout", lambda d: None)
-    sp.wait_outstreams()
-    ret = sp.wait()
-    assert ret
-    assert ret == -9
+    @pytest.mark.parametrize("stream", ["stdout", "stderr"])
+    def test_outstreams_linetimeout(self, stream):
+        sp = StreamProcess(["python", "-c",
+            "import time ; time.sleep(5)"], linetimeout=0.01)
+        sp.copy_outstream(stream, lambda d: None)
+        sp.wait_outstreams()
+        ret = sp.wait()
+        assert ret
+        assert ret == -9
