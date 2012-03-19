6ee9181db5e52e7aedf527b3c35451bf54550fac
holger krekel <holger@merlinux.eu>
2012-03-19 06:31:28 -0700

    moving towards release packaging

--- setup.py
+++ setup.py
@@ -2,18 +2,34 @@ import os, sys
 
 from setuptools import setup
 
+long_description="""
+What is detox?
+==========================
+
+detox is the distributed version of "tox".  It makes efficient use of multiple
+CPUs by running all possible activities in parallel.  It has the same options
+and configuration that tox has so after installation can just run::
+
+    detox
+
+in the same way you would run ``tox``.  For documentation and examples for tox
+please refer to the `tox home page`_.
+
+.. _`tox home page`: http://tox.testrun.org/
+"""
+
 def main():
     setup(
         name='detox',
-        description='distributing tests via tox',
-        #long_description = open('README.txt').read(),
-        version='0.4.dev3',
+        description='distributing activities of the tox tool',
+        long_description = long_description,
+        version='0.4.dev4',
         url='http://bitbucket.org/hpk42/detox',
         license='AGPL3',
         platforms=['unix', 'linux', 'osx', 'cygwin', 'win32'],
         author='holger krekel',
         author_email='holger at merlinux.eu',
-        classifiers=['Development Status :: 6 - Mature',
+        classifiers=['Development Status :: 4 - Beta',
                      'Intended Audience :: Developers',
                      'License :: OSI Approved :: MIT License',
                      'Operating System :: POSIX',
@@ -24,9 +40,9 @@ def main():
                      'Topic :: Utilities',
                      'Programming Language :: Python',
                      'Programming Language :: Python :: 3'],
-        packages=['detox',
-        ],
-        install_requires=['tox>=1.3', 'py>=1.4.7.dev3', 'eventlet>=0.9.16',],
+        packages=['detox', ],
+        install_requires=['tox>=1.4.dev3',
+            'py>=1.4.7.dev3', 'eventlet>=0.9.16',],
         entry_points={'console_scripts': 'detox=detox.main:main'},
     )
 
