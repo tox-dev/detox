a1102c18cba81894cde4a25348078faac9b75af0
holger krekel <holger@merlinux.eu>
2013-09-04 13:28:43 +0200

    use easy_install for detox own tests and mention that only py26,py27 are supported

--- setup.py
+++ setup.py
@@ -15,6 +15,15 @@ and configuration that tox has so after installation can just run::
 in the same way and with the same options with which you would run
 ``tox``, see the `tox home page`_ for more info.
 
+Please file issues as "tox" issues using the "detox" component:
+
+    https://bitbucket.org/hpk42/tox/issues
+
+.. note::
+
+    detox runs only on python2.6 and python2.7 (but supports creation of
+    python3 and all environments supported of the underlying "tox" command)
+
 .. _`tox home page`: http://tox.testrun.org/
 """
 
@@ -23,7 +32,7 @@ def main():
         name='detox',
         description='distributing activities of the tox tool',
         long_description = long_description,
-        version='0.9.2',
+        version='0.9.3',
         url='http://bitbucket.org/hpk42/detox',
         license='AGPL3',
         platforms=['unix', 'linux', 'osx', 'cygwin', 'win32'],
