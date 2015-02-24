33133b06d7889eeca982a6dcc877bb835c66528c
holger krekel <holger@merlinux.eu>
2015-02-24 16:47:43 +0100

    0.9.4 releaes, add changelog, bump version

--- setup.py
+++ setup.py
@@ -30,9 +30,9 @@ Please file issues as "tox" issues using the "detox" component:
 def main():
     setup(
         name='detox',
-        description='distributing activities of the tox tool',
+        description='distributing activities of the tox tool (py2 only)',
         long_description = long_description,
-        version='0.9.4.dev1',
+        version='0.9.4',
         url='http://bitbucket.org/hpk42/detox',
         license='MIT',
         platforms=['unix', 'linux', 'osx', 'cygwin', 'win32'],
@@ -50,8 +50,8 @@ def main():
                      'Programming Language :: Python',
                      ],
         packages=['detox', ],
-        install_requires=['tox>=1.6.1',
-            'py>=1.4.13', 'eventlet>=0.9.16',],
+        install_requires=['tox>=1.9.0',
+            'py>=1.4.13', 'eventlet>=0.15.0',],
         entry_points={'console_scripts': 'detox=detox.main:main'},
     )
 
