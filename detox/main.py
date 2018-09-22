e6722eb6ae6dd8bd2d44f552467be9459f050de5
Oliver Bestwalter <oliver.bestwalter@avira.com>
2018-09-22 10:53:30 +0200

    formatting, remove long dead code

--- detox/main.py
+++ detox/main.py
@@ -1,23 +1,18 @@
 import sys
-import py
-import detox
+
 from detox.proc import Detox
+
 
 def parse(args):
     from tox.session import prepare
     return prepare(args)
 
+
 def main(args=None):
     if args is None:
         args = sys.argv[1:]
     config = parse(args)
-    #now = py.std.time.time()
     detox = Detox(config)
     detox.startloopreport()
     retcode = detox.runtestsmulti(config.envlist)
-    #elapsed = py.std.time.time() - now
-    #cumulated = detox.toxsession.report.cumulated_time
-    #detox.toxsession.report.line(
-    #    "detox speed-up: %.2f (elapsed %.2f, cumulated %.2f)" % (
-    #        cumulated / elapsed, elapsed, cumulated), bold=True)
     raise SystemExit(retcode)
