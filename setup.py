cb8f905edd6660b5022696ceebc12a89cf340c08
Oliver Bestwalter <oliver.bestwalter@avira.com>
2018-09-22 12:43:25 +0200

    post release fix to also have info about new issue tracker in PyPI

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ def main():
         name='detox',
         description='distributing activities of the tox tool',
         long_description=long_description,
-        version='0.14',
+        version='0.14.post1',  # Note: keep in sync with detox/__init__.py
         url='https://github.com/tox-dev/detox',
         license='MIT',
         platforms=['unix', 'linux', 'osx', 'cygwin', 'win32'],
